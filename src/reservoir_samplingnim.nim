6c6905f04415b8f7b621758bebb7430f044271f0
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-11-18 10:33:43 -0500

    First working version.

--- src/reservoir_samplingnim.nim
+++ src/reservoir_samplingnim.nim
@@ -6,33 +6,27 @@ import std/enumerate
 #import std/json
 import std/random
 from std/times import getTime, nanosecond
+from system/io import stdin
+#import std/os
 
 
 
-proc r(numSample: int = 3): seq[string] =
+proc r(sampleSize: int = 3): seq[string] =
   let now1 = getTime()
   var rng = initRand(now1.nanosecond)
 
-  var reservoir: seq[string] = @[]
-
   # [enumerate lines in stdin](https://stackoverflow.com/a/65146313)
-  #for (i, sample) in enumerate(stdin.lines):
-  #let f = open(stdin)
-  #defer: f.close()
-  #for s_index, sample in enumerate(lines(f)):
-  #for s_index, sample in enumerate(lines(stdin)):
-  let s_index: int=0
-  for sample in stdin.lines:
-    if s_index < numSample:
-      reservoir.add(sample)
+  for sIndex, sample in enumerate(lines(stdin)):
+    if sIndex < sampleSize:
+      result.add(sample)
     else:
-      const k = rng.rand(s_index)
-      if k < sample_size:
-        reservoir[k] = sample
+      let k: int = rng.rand(sIndex)
+      if k < sampleSize:
+        result[k] = sample
 
 
 
 when isMainModule:
-  const samples = r(3)
+  let samples = r(3)
   for sample in samples:
     echo sample
