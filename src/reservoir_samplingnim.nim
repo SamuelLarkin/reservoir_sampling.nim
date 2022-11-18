f3bb0b273aef78c1789e093f663dca191bc8f104
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-11-18 10:19:50 -0500

    Still can't compile.

--- src/reservoir_samplingnim.nim
+++ src/reservoir_samplingnim.nim
@@ -3,20 +3,30 @@
 # uses this file as the main entry point of the application.
 
 import std/enumerate
-import std/json
+#import std/json
 import std/random
+from std/times import getTime, nanosecond
 
 
 
 proc r(numSample: int = 3): seq[string] =
-  var numSample: int = 3
+  let now1 = getTime()
+  var rng = initRand(now1.nanosecond)
+
   var reservoir: seq[string] = @[]
 
-  for i, sample in enumerate(stdin.lines):
-    if i < numSample:
+  # [enumerate lines in stdin](https://stackoverflow.com/a/65146313)
+  #for (i, sample) in enumerate(stdin.lines):
+  #let f = open(stdin)
+  #defer: f.close()
+  #for s_index, sample in enumerate(lines(f)):
+  #for s_index, sample in enumerate(lines(stdin)):
+  let s_index: int=0
+  for sample in stdin.lines:
+    if s_index < numSample:
       reservoir.add(sample)
     else:
-      const k = rand(i)
+      const k = rng.rand(s_index)
       if k < sample_size:
         reservoir[k] = sample
 
